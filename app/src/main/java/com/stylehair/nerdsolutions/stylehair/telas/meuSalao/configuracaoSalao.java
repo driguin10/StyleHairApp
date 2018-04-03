05895a19d68f7b036ee32df18b395d100d6db0a9
Rodrigo <driguin_10@hotmail.com>
2018-04-03 14:37:16 -0300

    atualizacao

--- app/src/main/java/com/stylehair/nerdsolutions/stylehair/telas/meuSalao/configuracaoSalao.java
+++ app/src/main/java/com/stylehair/nerdsolutions/stylehair/telas/meuSalao/configuracaoSalao.java
@@ -1,11 +1,20 @@
 package com.stylehair.nerdsolutions.stylehair.telas.meuSalao;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 import com.stylehair.nerdsolutions.stylehair.R;
+
+import java.util.Calendar;
 
 public class configuracaoSalao extends AppCompatActivity {
 
@@ -18,7 +27,47 @@ public class configuracaoSalao extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
         getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
         getSupportActionBar().setTitle("Configuração do Salão");
+
+
+        Button bt = (Button) findViewById(R.id.btshoww);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(v);
+            }
+        });
     }
+
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new timerPick();
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    public static class timerPick extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+        }
+    }
+
+
+
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
