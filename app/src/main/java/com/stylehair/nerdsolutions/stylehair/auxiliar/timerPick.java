cf2866e59ce30342e9fd7889bd8b2869386c7b6c
Rodrigo <driguin_10@hotmail.com>
2018-04-03 22:26:38 -0300

    atualizacao

---
+++ app/src/main/java/com/stylehair/nerdsolutions/stylehair/auxiliar/timerPick.java
@@ -0,0 +1,60 @@
+package com.stylehair.nerdsolutions.stylehair.auxiliar;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.widget.TimePicker;
+
+import com.stylehair.nerdsolutions.stylehair.R;
+
+
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class timerPick extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    public int id;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextInputLayout imput = ((TextInputLayout) getActivity().findViewById(id));
+        String h = hourOfDay + ":" + minute;
+        SimpleDateFormat formatador = new SimpleDateFormat("HH:mm");
+        Date data = null;
+        try {
+            data = formatador.parse(h);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Time time = new Time(data.getTime());
+        String[] hora = time.toString().split(":");
+        imput.getEditText().setText(hora[0]+":"+hora[1]);
+    }
+
+
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+}
--- app/src/main/java/com/stylehair/nerdsolutions/stylehair/telas/meuSalao/configuracaoSalao.java
+++ app/src/main/java/com/stylehair/nerdsolutions/stylehair/telas/meuSalao/configuracaoSalao.java
@@ -3,20 +3,38 @@ package com.stylehair.nerdsolutions.stylehair.telas.meuSalao;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 import com.stylehair.nerdsolutions.stylehair.R;
+import com.stylehair.nerdsolutions.stylehair.auxiliar.timerPick;
 
 import java.util.Calendar;
 
 public class configuracaoSalao extends AppCompatActivity {
+
+     ImageButton horaSegE;
+     ImageButton horaSegS;
+     TextInputLayout txtSegE;
+     TextInputLayout txtSegS;
+     ToggleButton btStatusSegunda;
+
+    ImageButton horaTerE;
+    ImageButton horaTerS;
+    TextInputLayout txtTerE;
+    TextInputLayout txtTerS;
+    ToggleButton btStatusTerca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,44 +46,117 @@ public class configuracaoSalao extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
         getSupportActionBar().setTitle("Configuração do Salão");
 
+        txtSegE = (TextInputLayout) findViewById(R.id.txt_hora_segE);
+        txtSegS = (TextInputLayout) findViewById(R.id.txt_hora_segS);
+        btStatusSegunda = (ToggleButton) findViewById(R.id.bt_folga_seg);
+        horaSegE = (ImageButton) findViewById(R.id.pesquisa_hora_segE);
+        horaSegS = (ImageButton) findViewById(R.id.pesquisa_hora_segS);
 
-        Button bt = (Button) findViewById(R.id.btshoww);
-        bt.setOnClickListener(new View.OnClickListener() {
+        txtTerE = (TextInputLayout) findViewById(R.id.txt_hora_terE);
+        txtTerS = (TextInputLayout) findViewById(R.id.txt_hora_terS);
+        btStatusTerca = (ToggleButton) findViewById(R.id.bt_folga_ter);
+        horaTerE = (ImageButton) findViewById(R.id.pesquisa_hora_terE);
+        horaTerS = (ImageButton) findViewById(R.id.pesquisa_hora_terS);
+
+        txtSegE.setEnabled(false);
+        txtSegS.setEnabled(false);
+
+        txtTerE.setEnabled(false);
+        txtTerS.setEnabled(false);
+
+        btStatusSegunda.setChecked(true);
+        btStatusTerca.setChecked(true);
+
+        muda(true,txtSegE,txtSegS,horaSegE,horaSegS,btStatusSegunda);
+        muda(true,txtTerE,txtTerS,horaTerE,horaTerS,btStatusTerca);
+
+        horaSegE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showTimePickerDialog(v);
+                showTimePickerDialog(v,R.id.txt_hora_segE);
+            }
+        });
+        horaSegS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(v,R.id.txt_hora_segS);
+            }
+        });
+
+        horaTerE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(v,R.id.txt_hora_terE);
+            }
+        });
+        horaTerS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(v,R.id.txt_hora_terS);
+            }
+        });
+
+
+
+        btStatusSegunda.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    muda(true,txtSegE,txtSegS,horaSegE,horaSegS,btStatusSegunda);
+                else
+                    muda(false,txtSegE,txtSegS,horaSegE,horaSegS,btStatusSegunda);
+            }
+        });
+
+        btStatusTerca.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    muda(true,txtTerE,txtTerS,horaTerE,horaTerS,btStatusTerca);
+                else
+                    muda(false,txtTerE,txtTerS,horaTerE,horaTerS,btStatusTerca);
             }
         });
     }
 
+    void muda(boolean status,TextInputLayout idtxtE,TextInputLayout idtxtS,ImageButton btE, ImageButton btS,ToggleButton btfolga)
+    {
+        if(status)
+        {
+            btfolga.setBackground(getResources().getDrawable(R.drawable.bt_folga_on));
+            idtxtE.setClickable(true);
+            idtxtE.setAlpha(1);
+            idtxtS.setClickable(true);
+            idtxtS.setAlpha(1);
+            btE.setEnabled(true);
+            btE.setClickable(true);
+            btE.setAlpha(1f);
+            btS.setEnabled(true);
+            btS.setClickable(true);
+            btS.setAlpha(1f);
+        }
+        else
+        {
+            btfolga.setBackground(getResources().getDrawable(R.drawable.bt_folga_off));
+            idtxtE.setClickable(false);
+            idtxtE.setAlpha(.3f);
+            idtxtS.setClickable(false);
+            idtxtS.setAlpha(.3f);
+            btE.setEnabled(false);
+            btE.setClickable(false);
+            btE.setAlpha(.3f);
+            btS.setEnabled(false);
+            btS.setClickable(false);
+            btS.setAlpha(.3f);
+        }
+    }
 
-    public void showTimePickerDialog(View v) {
-        DialogFragment newFragment = new timerPick();
+    public void showTimePickerDialog(View v,int idCampo) {
+        timerPick tim = new timerPick();
+        tim.setId(idCampo);
+        DialogFragment newFragment = tim;
         newFragment.show(getFragmentManager(), "timePicker");
     }
-
-    public static class timerPick extends DialogFragment
-            implements TimePickerDialog.OnTimeSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-
-            // Create a new instance of TimePickerDialog and return it
-            return new TimePickerDialog(getActivity(), this, hour, minute,
-                    DateFormat.is24HourFormat(getActivity()));
-        }
-
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // Do something with the time chosen by the user
-        }
-    }
-
-
-
 
 
     @Override
